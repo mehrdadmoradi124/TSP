f7457cf5427dc111bf2074123f69a3c42f477155
Baig, Faisal <faisalb875@gatech.edu>
2023-11-30 14:44:48 -0500

    Local search - hill climbing,helper methods w/docs

--- code/tsp.py
+++ code/tsp.py
@@ -1,6 +1,8 @@
 import argparse
 import time
-# import multiprocessing
+
+from LocalTSPhelper import random_walk, hill_climbing
+# import multiprocessing - for use in timing if we can't figure out another way
 
 def exact(adjacency_matrix):
     pass
@@ -8,8 +10,46 @@ def exact(adjacency_matrix):
 def approximate_search(adjacency_matrix):
     pass
 
-def local_search(adjacency_matrix, seed):
-    return 10, [1, 2, 3 , 4, 5]
+
+def local_search(adjacency_matrix, time_limit, seed):
+    """
+    Performs a local search for TSP, problem is represented by an 2D adjacency matrix.
+    Utilized random resets and 'hill climbing' (optimization based on neighborhood function) 
+    to improve on initial solution. To track how the model optimizes a solution uncomment 
+    the lines marked in the function. Helper functions used can be found in the LocalTSPHelper.py 
+    funciton.
+
+    Parameters:
+    - adjacency_matrix: Represents the graph
+    - time_limit: The maximum duration (in seconds) for the search to run
+    - seed: Value to allow for consistent randomization results
+
+    Returns:
+    - quality: The sum of edge weights/distances between nodes in solution
+    - ids: List of node ids representing TSP solution
+    """
+    start_time = time.time()
+    quality, ids = random_walk(adjacency_matrix, seed)
+    # print(quality) # Uncomment this to see starting quality score
+    while True:
+        if time.time() - start_time >= time_limit:
+            break
+        old_quality, old_ids = quality, ids
+        seed += 1
+        hquality, hids = hill_climbing(quality, ids, adjacency_matrix, seed)
+        if hquality < quality:
+            # print('Improved from:' + quality + " to " + hquality) # Uncomment this to see how hill climbing improves quality
+            quality = hquality
+            ids = hids
+        if old_ids == ids and quality == old_quality:
+            seed += 1
+            random_quality, random_ids = random_walk(adjacency_matrix, seed)
+            if random_quality < quality:
+                quality = random_quality
+                ids = random_ids
+                # print(quality) # Uncomment this line to track how often ransom reset improves quality
+    # print(quality) # Uncomment this to see final quality score
+    return quality, ids
 
 def euclidean_distance(p1, p2):
     x1, y1 = p1
@@ -32,7 +72,9 @@ def tsp_parser(inst):
     with open('data/' + inst, 'r') as file:
         for line in file:
             words = line.split()
-            if words[0] == 'NAME:':
+            if len(words) == 0:
+                pass
+            elif words[0] == 'NAME:':
                 city = words[1]
             elif words[0] == 'DIMENSION:':
                 dimension = words[1]
@@ -56,7 +98,7 @@ def main(inst, alg, time, seed):
     # parse the .TSP file create 2D adjacency matrix for graph representation
     city, dimension, edge_weight_type, locations = tsp_parser(inst)
     adjacency_matrix = construct_graph(locations)
-    print(adjacency_matrix)
+    # print(adjacency_matrix)
     if alg == 'BF':
         quality, ids = exact(adjacency_matrix)
         file_name = str(city) + "_" + str(alg) + "_" + str(time)
@@ -66,7 +108,7 @@ def main(inst, alg, time, seed):
         file_name = str(city) + "_" + str(alg) + "_" + str(seed)
         generate_solution(file_name, quality, ids)
     elif alg == 'LS':
-        quality, ids  = local_search(adjacency_matrix, seed)
+        quality, ids  = local_search(adjacency_matrix, int(time), int(seed))
         file_name = str(city) + "_" + str(alg) + "_" + str(time) + "_" + str(seed)
         generate_solution(file_name, quality, ids)
         
@@ -82,7 +124,10 @@ if __name__ == "__main__":
     main(args.inst, args.alg, args.time, args.seed)
 
 
+
 # python tsp.py -inst Atlanta.tsp -alg LS -time 600 -seed 42
+# python tsp.py -inst UKansasState.tsp -alg LS -time 10 -seed 42
+# python tsp.py -inst Berlin.tsp -alg LS -time 30 -seed 42     
 
 # created a working executable with PyInststaller
 # tsp.exe -inst Atlanta.tsp -alg LS -time 600 -seed 42
